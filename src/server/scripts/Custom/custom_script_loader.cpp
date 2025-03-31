14d8b870510b28ea8dffe247ab9c59159b01b678
Rodrigo <rodrigo99@live.nl>
2025-03-31 23:50:52 +0200

    Custom server config and custom script loader changes

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,10 +17,14 @@
 
 // This is where scripts' loading functions should be declared:
 // void MyExampleScript()
+void AddSC_stealth_speed_rogue();
+void AddSC_stealth_speed_druid();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
     // MyExampleScript()
+    AddSC_stealth_speed_rogue();
+    AddSC_stealth_speed_druid();
 }
